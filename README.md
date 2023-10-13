41a2ebf0b2994963957e7443f8626c97ececa158
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-13 23:20:33 +0600

    initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Espresso Coffee Shop
+
+This is a simple coffee shop , give a visit.
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-3xl font-rancho my-60" >Coffee Shop</h1>
+    </>
+  );
+}
+
+export default App;
