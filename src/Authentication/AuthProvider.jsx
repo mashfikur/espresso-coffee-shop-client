0ace95a20ce2cc7bafafe2b2dddbc9abf838784f
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-16 23:40:32 +0600

    user creation with email and firebase completed

--- src/Authentication/AuthProvider.jsx
+++ src/Authentication/AuthProvider.jsx
@@ -1,18 +1,31 @@
-import PropTypes from 'prop-types';
-import { createContext } from 'react';
+import PropTypes from "prop-types";
+import { createContext, useState } from "react";
+import auth from "../firebase/firebase.config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
 
-const AuthProvider = ({children}) => {
-    return (
-        <AuthContext.Provider>
-            {children}
-        </AuthContext.Provider>
-    );
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userInfo = {
+    user,
+    createUser
+  };
+
+  return (
+    <AuthContext.Provider value={userInfo}>
+        {children}
+    </AuthContext.Provider>
+  );
 };
 
 AuthProvider.propTypes = {
-    children:PropTypes.node,
+  children: PropTypes.node,
 };
 
 export default AuthProvider;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,75 @@
-
+import { useContext } from "react";
+import { AuthContext } from "../Authentication/AuthProvider";
+import toast from "react-hot-toast";
 
 const SignUp = () => {
-    return (
-        <div>
-           <h3>Create Your New account</h3> 
+  const { createUser } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const email = form.email.value;
+    const password = form.password.value;
+
+    // creating user
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        toast.success("User created Successfully");
+      })
+      .catch((error) => {
+        toast.error(error.code);
+      });
+  };
+
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign Up now!</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                  name="email"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                  name="password"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Sign Up</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
